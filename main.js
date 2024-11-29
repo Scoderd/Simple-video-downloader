be0d0d90ff63a4f0c53422f6397bb71fb306033c
Sadew Dewmika <sadewdewmika10@gmail.com>
2024-11-30 00:52:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+/* 
+var inp = ""
+document.getElementById("down1").onclick=function() {
+document.getElementById("pro1").style.display="block";
+inp = document.getElementById("input1").value;
+
+alert(inp);  
+}
+
+
+
+/* To hide the alert:
+
+
+// To check the input:
+const emp = ""
+document.getElementById("down").onclick=function(){
+   var inp = ""
+   document.getElementById("alert").classList.add("invisible");
+   inp = document.getElementById("input").value;
+   if (inp==emp) {
+      
+// This is to show the alert:
+      document.getElementById("alert").classList.add("visible");
+      
+// This is for error showing
+     document.getElementById("alert-text").innerHTML="Error: Please provide a input";
+   } else {
+      
+// To remove any exces alerts
+   
+   }
+}
+
+*/
+
+ 
+
+
